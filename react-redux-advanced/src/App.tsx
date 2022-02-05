d6e9e33cb1ab42249521793fe22182e1db2f0490
Nick <nsagias@gmail.com>
2022-02-04 20:47:52 -0500

    update to toggle cart from on App.tsx, clean dead code

--- react-redux-advanced/src/App.tsx
+++ react-redux-advanced/src/App.tsx
@@ -1,11 +1,14 @@
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { useSelector } from 'react-redux';
+import { RootState } from './components/store/index';
 
 function App() {
+  const toggle = useSelector((state: RootState) => state.uiSlice.cartIsVisible);
   return (
     <Layout>
-      <Cart />
+     {toggle && <Cart /> }
       <Products />
     </Layout>
   );
--- react-redux-advanced/src/components/Cart/CartButton.tsx
+++ react-redux-advanced/src/components/Cart/CartButton.tsx
@@ -1,21 +1,20 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { RootState, AppDispatch} from '../store/index';
+import { useDispatch } from 'react-redux';
+import { AppDispatch} from '../store/index';
 import { uiActions } from '../store/ui-slice';
 import { IToggleCartHandler } from './cart.models';
 import classes from './CartButton.module.css';
 
 const CartButton: React.FC = (props) => {
   const dispatch = useDispatch<AppDispatch>();
-  const toggle = useSelector((state: RootState) => state.uiSlice.cartIsVisible)
-  
+ 
 
   const toggleCartHandler: IToggleCartHandler = () => {
     dispatch ( uiActions.toggle() );
   }
 
   return (
-    <button className={classes.button}>
-      <span>My Cart</span>
+    <button className={classes.button} onClick={toggleCartHandler}>
+      <span>My Cart</span> 
       <span className={classes.badge}>1</span>
     </button>
   );
