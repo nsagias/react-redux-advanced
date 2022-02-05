1794eeb8984cc526c4863c46bdad0b18fdbaaa06
Nick <nsagias@gmail.com>
2022-02-04 20:20:54 -0500

    import useSelector, useDispatch, RootSTate, AppDispatch, use useDispatch and useSelector, create dispatch and toggle variable

--- react-redux-advanced/src/components/Layout/MainHeader.tsx
+++ react-redux-advanced/src/components/Layout/MainHeader.tsx
@@ -1,7 +1,14 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState, AppDispatch} from '../store/index';
+import { uiAction } from '../store/ui-slice';
 import CartButton from '../Cart/CartButton';
 import classes from './MainHeader.module.css';
 
+
 const MainHeader: React.FC = (props) => {
+  const dispatch = useDispatch<AppDispatch>();
+  const toggle = useSelector((state: RootState) => state.uiSlice.cartIsVisible)
+  
   return (
     <header className={classes.header}>
       <h1>ReduxCart</h1>
