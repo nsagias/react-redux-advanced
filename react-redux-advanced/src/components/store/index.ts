6e9d6609882ede52f76dacb8eaedafed2113f664
Nick <nsagias@gmail.com>
2022-02-04 20:48:25 -0500

    lint

--- react-redux-advanced/src/components/store/index.ts
+++ react-redux-advanced/src/components/store/index.ts
@@ -3,11 +3,9 @@ import uiSliceReducer from "./ui-slice";
 
 
 const store = configureStore({
-  reducer: {
-      uiSlice: uiSliceReducer
-    }
+  reducer: { uiSlice: uiSliceReducer }
   });
 
 export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
 export default store;
