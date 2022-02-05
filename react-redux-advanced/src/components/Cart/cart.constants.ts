ac681df26490a78bba8caca2f9d95017d5d2bd0a
Nick <nsagias@gmail.com>
2022-02-04 19:29:21 -0500

    refactor constants into their own components

---
+++ react-redux-advanced/src/components/Cart/cart.constants.ts
@@ -0,0 +1,4 @@
+
+export const CART_SLICE: string = "CART_SLICE";
+
+export const INITIAL_STATE_CART_SLICE = "";
---
+++ react-redux-advanced/src/components/UI/ui.constants.ts
@@ -0,0 +1,4 @@
+
+export const UI_SLICE: string = "UI_SLICE";
+
+export const INITIAL_STATE_UI_SLICE = { cartIsVisible: false };
