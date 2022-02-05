4471d4dbf09d668a199829e633def72d111321fd
Nick <nsagias@gmail.com>
2022-02-04 19:48:38 -0500

    import uiSliceReducer from ui-slice, create store with uiSlice, export defautl store, add types

--- react-redux-advanced/src/components/store/index.ts
+++ react-redux-advanced/src/components/store/index.ts
@@ -1,13 +1,13 @@
 import { createSlice, configureStore, PayloadAction} from "@reduxjs/toolkit";
+import uiSliceReducer from "./ui-slice";
 
 
+const store = configureStore({
+  reducer: {
+      uiSlice: uiSliceReducer
+    }
+  });
 
-
-// const store = configureStore({
-//   reducer: {
-
-//   });
-
-// export type RootState = ReturnType<typeof store.getState>
-// export type AppDispatch = typeof store.dispatch
-// export default store;
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+export default store;
