b4e94c92b7c812f94e2f0df305ca67f964abae2e
Nick <nsagias@gmail.com>
2022-02-04 19:41:40 -0500

    createSlice with import UI_SLICE, INITIAL_STATE_UI_SLICE, create reducer toggle, with IUISlice interface, export ui.Slice actions and uiSlice.reducer

--- react-redux-advanced/src/components/store/ui-slice.ts
+++ react-redux-advanced/src/components/store/ui-slice.ts
@@ -1,1 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit"; 
+import { IUISlice } from "../UI/card.models";
+import { UI_SLICE, INITIAL_STATE_UI_SLICE } from "../UI/ui.constants";
+
+const uiSlice = createSlice({
+  name: UI_SLICE,
+  initialState: INITIAL_STATE_UI_SLICE,
+  reducers: {
+    toggle(state: IUISlice):  void {
+      state.cartIsVisible = !state.cartIsVisible
+    }
+  } 
+});
+
+export const uiSliceAction  = uiSlice.actions;
+export default uiSlice.reducer;
