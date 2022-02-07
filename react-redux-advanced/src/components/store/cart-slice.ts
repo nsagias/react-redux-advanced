161cd5e65f5e997a5bf23ec185640d6f4c674e2f
Nick <nsagias@gmail.com>
2022-02-07 00:44:48 -0500

    creat initialstate const, create interfaces

--- react-redux-advanced/src/components/store/cart-slice.ts
+++ react-redux-advanced/src/components/store/cart-slice.ts
@@ -1,17 +1,45 @@
 import { createSlice } from "@reduxjs/toolkit"; 
 
+
+const INITIAL_STATE_CART_ITEMS= {
+  items: [{
+    id: "id", 
+    price: 0, 
+    quantity: 0, 
+    totalPrice: 0,
+    name: ""
+  }],
+  totalQuantity: 0
+};
+
+
+interface INewItem {
+  items: {
+    id: string;
+    price: number;
+    quantity: number;
+    totalPrice: number;
+    name: string;
+  }[];
+}
+
+
+interface INewItemAction {
+    payload: any;
+    type: string;
+}
+
+
 const cartSlice = createSlice({
   name: 'cart',
-  initialState: {
-    items: [],
-    totalQuantity: 0
-  }, reducers: {
-    addItemtoCart(state, action) {
+  initialState: INITIAL_STATE_CART_ITEMS, 
+  reducers: {
+    addItemtoCart(state: INewItem , action: INewItemAction): void {
       const newItem = action.payload;
       const existingItems = state.items.find(item => item.id === newItem.id);
       if (!existingItems) {
         state.items.push({ 
-          itemId: newItem.id, 
+          id: newItem.id, 
           price: newItem.price, 
           quantity: 1, 
           totalPrice: newItem.price,
