523c3481a8991ab15f62b7d32a426605a90e4087
Nick <nsagias@gmail.com>
2022-02-06 23:57:22 -0500

    Create cartSlice without typescript

--- react-redux-advanced/src/components/store/cart-slice.ts
+++ react-redux-advanced/src/components/store/cart-slice.ts
@@ -1,1 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit"; 
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+    totalAmount: 0
+  }, reducers: {
+
+  }
+});
+
+export const cartActions = cartSlice.actions;
+export default cartSlice.reducer;
